286487251db81eda5d6ba3efeefdffea9b1041dd
zhumengqi <mickey.zh.cn@gmail.com>
2017-04-26 19:14:09 +0800

    添加spring demo程序，部署在zhumengqi.cn:8080/***可以提供restful接口

---
+++ springdemo/docs/readme.md
@@ -0,0 +1,5 @@
+## 上传
+scp /home/mike/workspace/out-of-work/www.zhumengqi.cn/springdemo/target/springdemo-1.0-SNAPSHOT.jar root@zhumengqi.cn:/root
+
+##访问
+http://zhumengqi.cn:8080/greeting
---
+++ springdemo/src/main/java/cn/zhumengqi/sdemo/controller/Application.java
@@ -0,0 +1,15 @@
+package cn.zhumengqi.sdemo.controller;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by zhumengqi on 17-4-26.
+ */
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
